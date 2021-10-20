6e57ae7501ed7dcddc042104a84165643387c0f7
Edikan Bassey <60517569+Mestareddy@users.noreply.github.com>
2021-10-20 13:35:11 +0100

    v2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
