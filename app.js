bfb60bd04060a5e9faae84e9b779273bb422b48c
Edikan Bassey <60517569+Mestareddy@users.noreply.github.com>
2021-10-21 09:22:09 +0100

    update

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000 || process.env.PORT
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-require('dotenv').config()
-const notFound = require('./middleware/not-found')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
-app.use(express.static('./public'))
-app.use(express.json())
+
+app.use(express.static('./public'));
+app.use(express.json());
 
 // routes
-app.use('/api/v1/tasks',tasks)
 
-app.use(notFound)
+app.use('/api/v1/tasks', tasks);
 
-const start = async () =>{
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => console.log('Example app listening on port: ' + port))
-    } catch (error) {
-        console.log(error);
-    }
-}
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
-start()
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-    return mongoose
-        .connect(url, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        })
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
 }
 
-module.exports = connectDB;
+module.exports = connectDB
